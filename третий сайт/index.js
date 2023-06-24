59d6d444332459fccb75b311f38c4d37b303ca53
Шарадона <pasha.kachanovskiy@mail.ru>
2023-06-25 00:52:07 +0500

    сделал 2 блок

--- третий сайт/index.js
+++ третий сайт/index.js
@@ -4,3 +4,10 @@
   document.querySelector(".close").addEventListener("click", function () {
     document.querySelector(".kontakt-inform-menu").classList.add("hidden")
   })
+
+  const tagsForGold = document.querySelectorAll(".little-theatre p, h3");
+  tagsForGold.forEach((tagForGold) => {
+    let arr = tagForGold.textContent.split(' ');
+    arr[0] = '<span>' + arr[0] + '</span>';
+    tagForGold.innerHTML = arr.join(' ');
+  });
